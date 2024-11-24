450f0eddab8528ba3c72684e719a6ff6b85775fa
deathrjj <robert.morgan527@gmail.com>
2024-11-24 20:57:53 +0000

    linuxbrew

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,9 @@ set -Ux fish_greeting ""
 if test -d /opt/homebrew/bin
     fish_add_path /opt/homebrew/bin
 end
+if test -d ~/linuxbrew/.linuxbrew/bin
+    fish_add_path ~/linuxbrew/.linuxbrew/bin
+end
 
 
 if status is-interactive;
